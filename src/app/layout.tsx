fe89950dc62054c86ad9939fed8ddf496ac093fd
Lavender Oyugi <Lavenderoyugi1@gmail.com>
2026-07-28 06:20:05 +0200

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Providers } from "@/components/Providers";
 import { Geist, Geist_Mono } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,10 +29,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+     <body className="min-h-full flex flex-col">
   <Providers>
     {children}
   </Providers>
+
+  <GoogleAnalytics gaId="G-JKF89RMDXR" />
 </body>
     </html>
   );
