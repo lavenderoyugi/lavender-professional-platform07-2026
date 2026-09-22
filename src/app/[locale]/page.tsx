9e2b132eacfc2c78a392c434f751c6ca26d9caab
Lavender Oyugi <Lavenderoyugi1@gmail.com>
2026-09-22 22:23:41 +0200

    Refactor Home function to redirect by locale
    Removed components from Home function and added redirect based on locale.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,31 +1,11 @@
-"use client";
+import { redirect } from "next/navigation";
 
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
 
-
-import HeroSection from "@/components/HeroSection";
-import ProfessionalHighlights from "@/components/ProfessionalHighlights/ProfessionalHighlights";
-import JourneySection from "@/components/JourneySection";
-
-import ContactSection from "@/components/ContactSection";
-import Navbar from "@/components/Navbar";
-
-export default function Home() {
-  
-  
-
-  return (
-  <main className="min-h-screen bg-black text-white">
-
-    <Navbar />
-
-    <HeroSection />
-
-    <ProfessionalHighlights />
-
-    <JourneySection />
-
-    <ContactSection />
-
-  </main>
-);
+  redirect(`/${locale}/finds`);
 }
